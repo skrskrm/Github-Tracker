01de5866f5707336abea1ba7b49a472bfd75fbb5
skrskrm <50164945+skrskrm@users.noreply.github.com>
2022-09-20 23:51:31 -0700

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Tracker</h1>
+      <p className="ml-2">Version 1.0.1</p>
+      <p className="text-1xl ml-2"> By Mingyang Wang </p>
+    </div>
+  )
+}
+
+export default About
